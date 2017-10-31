d2e800c4dde08f2d4a7367feb0703059e3ea6c5f
childe <heychilde@gmail.com>
2017-10-31 12:01:18 +0800

    remove comment

--- filter.go
+++ filter.go
@@ -7,11 +7,6 @@ type Filter interface {
 func getFilter(filterType string, config map[interface{}]interface{}) Filter {
 	switch filterType {
 	case "Add":
-		//t := reflect.TypeOf(AddFilter{})
-		//pt := reflect.New(t)
-		//b := pt.Interface().(Filter)
-		//b.init(config)
-		//return b
 		return NewAddFilter(config)
 	case "Grok":
 		return NewGrokFilter(config)
