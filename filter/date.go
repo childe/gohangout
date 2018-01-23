43c275042c35548553078f1a8fba97689052e622
childe <heychilde@gmail.com>
2018-01-23 17:35:59 +0800

    add Date filter

---
+++ filter/date.go
@@ -0,0 +1,192 @@
+package filter
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/childe/gohangout/field_setter"
+	"github.com/childe/gohangout/value_render"
+	"github.com/golang/glog"
+)
+
+type DateParser interface {
+	Parse(interface{}) (time.Time, error)
+}
+
+type FormatParser struct {
+	format   string
+	location *time.Location
+}
+
+var MustStringTypeError = errors.New("timestamp field must be string")
+
+func (dp *FormatParser) Parse(t interface{}) (time.Time, error) {
+	var (
+		rst time.Time
+	)
+	if reflect.TypeOf(t).Kind() != reflect.String {
+		return rst, MustStringTypeError
+	}
+	if dp.location == nil {
+		return time.Parse(dp.format, t.(string))
+	}
+	return time.ParseInLocation(dp.format, t.(string), dp.location)
+}
+
+type UnixParser struct{}
+
+func (p *UnixParser) Parse(t interface{}) (time.Time, error) {
+	var (
+		rst time.Time
+	)
+	if reflect.TypeOf(t).String() == "json.Number" {
+		t1, err := t.(json.Number).Int64()
+		if err != nil {
+			return rst, err
+		}
+		return time.Unix(t1, 0), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.String {
+		t1, err := strconv.Atoi(t.(string))
+		if err != nil {
+			return rst, err
+		}
+		return time.Unix(int64(t1), 0), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.Int {
+		t1 := int64(t.(int))
+		return time.Unix(t1, 0), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.Int64 {
+		t1 := t.(int64)
+		return time.Unix(t1, 0), nil
+	}
+	return rst, fmt.Errorf("%s unknown type:%s", t, reflect.TypeOf(t).String())
+}
+
+type UnixMSParser struct{}
+
+func (p *UnixMSParser) Parse(t interface{}) (time.Time, error) {
+	var (
+		rst time.Time
+	)
+	if reflect.TypeOf(t).String() == "json.Number" {
+		t1, err := t.(json.Number).Int64()
+		if err != nil {
+			return rst, err
+		}
+		return time.Unix(t1/1000, t1%1000*1000000), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.String {
+		t1, err := strconv.Atoi(t.(string))
+		if err != nil {
+			return rst, err
+		}
+		t2 := int64(t1)
+		return time.Unix(t2/1000, t2%1000*1000000), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.Int {
+		t1 := int64(t.(int))
+		return time.Unix(t1/1000, t1%1000*1000000), nil
+	}
+	if reflect.TypeOf(t).Kind() == reflect.Int64 {
+		t1 := t.(int64)
+		return time.Unix(t1/1000, t1%1000*1000000), nil
+	}
+	return rst, fmt.Errorf("%s unknown type:%s", t, reflect.TypeOf(t).String())
+}
+
+func getDateParser(format string, l *time.Location) DateParser {
+	if format == "UNIX" {
+		return &UnixParser{}
+	}
+	if format == "UNIX_MS" {
+		return &UnixMSParser{}
+	}
+	if format == "RFC3339" {
+		return &FormatParser{time.RFC3339, l}
+	}
+	return &FormatParser{format, l}
+}
+
+type DateFilter struct {
+	BaseFilter
+
+	config      map[interface{}]interface{}
+	dateParsers []DateParser
+	overwrite   bool
+	src         string
+	srcVR       value_render.ValueRender
+	target      string
+	targetFS    field_setter.FieldSetter
+}
+
+func NewDateFilter(config map[interface{}]interface{}) *DateFilter {
+	plugin := &DateFilter{
+		BaseFilter:  BaseFilter{config},
+		config:      config,
+		overwrite:   true,
+		dateParsers: make([]DateParser, 0),
+	}
+
+	if overwrite, ok := config["overwrite"]; ok {
+		plugin.overwrite = overwrite.(bool)
+	}
+
+	if srcValue, ok := config["src"]; ok {
+		plugin.src = srcValue.(string)
+	} else {
+		plugin.src = "message"
+	}
+	plugin.srcVR = value_render.GetValueRender2(plugin.src)
+
+	if targetI, ok := config["target"]; ok {
+		plugin.target = targetI.(string)
+	} else {
+		plugin.target = "@timestamp"
+	}
+	plugin.targetFS = field_setter.NewFieldSetter(plugin.target)
+
+	var (
+		location *time.Location
+		err      error
+	)
+	if locationI, ok := config["location"]; ok {
+		location, err = time.LoadLocation(locationI.(string))
+		if err != nil {
+			glog.Fatal("load location error:%s", err)
+		}
+	} else {
+		location = nil
+	}
+	if formats, ok := config["formats"]; ok {
+		for _, formatI := range formats.([]interface{}) {
+			plugin.dateParsers = append(plugin.dateParsers, getDateParser(formatI.(string), location))
+		}
+	} else {
+		glog.Fatal("formats must be set in date filter plugin")
+	}
+
+	return plugin
+}
+
+func (plugin *DateFilter) Process(event map[string]interface{}) (map[string]interface{}, bool) {
+	inputI := plugin.srcVR.Render(event)
+	if inputI == nil {
+		glog.V(5).Infof("(%s) not in event", plugin.src)
+		return event, false
+	}
+
+	for _, dp := range plugin.dateParsers {
+		t, err := dp.Parse(inputI)
+		if err == nil {
+			event = plugin.targetFS.SetField(event, t, "", plugin.overwrite)
+			return event, true
+		}
+	}
+	return event, false
+}
---
+++ filter/date_test.go
@@ -0,0 +1,60 @@
+package filter
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestDateFilter(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	config["location"] = "Asia/Shanghai"
+	config["src"] = "@timestamp"
+	config["formats"] = []interface{}{"RFC3339", "UNIX"}
+	f := NewDateFilter(config)
+
+	event := make(map[string]interface{})
+	event["@timestamp"] = time.Now().Unix()
+	t.Log(event)
+
+	event, ok := f.Process(event)
+	t.Log(event)
+
+	if ok == false {
+		t.Error("fail")
+	}
+
+	event["@timestamp"] = strconv.Itoa((int)(time.Now().Unix()))
+	t.Log(event)
+
+	event, ok = f.Process(event)
+	t.Log(event)
+
+	if ok == false {
+		t.Error("fail")
+	}
+
+	event["@timestamp"] = "2018-01-23T17:06:05+08:00"
+	t.Log(event)
+
+	event, ok = f.Process(event)
+	t.Log(event)
+
+	if ok == false {
+		t.Error("fail")
+	}
+
+	config["location"] = "Etc/UTC"
+	config["formats"] = []interface{}{"2006-01-02T15:04:05"}
+	f = NewDateFilter(config)
+	event["@timestamp"] = "2018-01-23T17:06:05"
+	t.Log(event)
+
+	event, ok = f.Process(event)
+	t.Log(event)
+
+	if ok == false {
+		t.Error("fail")
+	}
+
+}
