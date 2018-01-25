26bfdf918b627f1028e494a3b8ce166cba3c23c1
childe <heychilde@gmail.com>
2018-01-25 18:37:30 +0800

    new file:   filter/drop.go

---
+++ filter/drop.go
@@ -0,0 +1,18 @@
+package filter
+
+type DropFilter struct {
+	BaseFilter
+	config map[interface{}]interface{}
+}
+
+func NewDropFilter(config map[interface{}]interface{}) *DropFilter {
+	plugin := &DropFilter{
+		BaseFilter: NewBaseFilter(config),
+		config:     config,
+	}
+	return plugin
+}
+
+func (plugin *DropFilter) Process(event map[string]interface{}) (map[string]interface{}, bool) {
+	return nil, true
+}
