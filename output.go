c2e74dbf9e582ba3de2c8b7b9386fe968f351039
childe <heychilde@gmail.com>
2018-01-25 18:37:50 +0800

    format

--- output.go
+++ output.go
@@ -7,10 +7,6 @@ type Output interface {
 func getOutput(outputType string, config map[interface{}]interface{}) Output {
 	switch outputType {
 	case "Stdout":
-		//t := reflect.TypeOf(StdoutOutput{})
-		//pt := reflect.New(t)
-		//b := pt.Interface().(Output)
-		//return b
 		return NewStdoutOutput(config)
 	case "Elasticsearch":
 		return NewElasticsearchOutput(config)
