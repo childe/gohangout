b0a5d64de04b87e6d566927bc08e81872e339a9c
childe <heychilde@gmail.com>
2018-01-28 02:57:44 +0800

    set @timestamp to time.Now().UTC()

--- codec/json_decoder.go
+++ codec/json_decoder.go
@@ -14,7 +14,7 @@ type JsonDecoder struct {
 
 func (jd *JsonDecoder) Decode(s string) map[string]interface{} {
 	rst := make(map[string]interface{})
-	rst["@timestamp"] = time.Now().UnixNano() / 1000000
+	rst["@timestamp"] = time.Now().UTC()
 	d := json.NewDecoder(strings.NewReader(s))
 	d.UseNumber()
 	err := d.Decode(&rst)
--- codec/plain_decoder.go
+++ codec/plain_decoder.go
@@ -7,7 +7,7 @@ type PlainDecoder struct {
 
 func (d *PlainDecoder) Decode(s string) map[string]interface{} {
 	rst := make(map[string]interface{})
-	rst["@timestamp"] = time.Now().UnixNano() / 1000000
+	rst["@timestamp"] = time.Now().UTC()
 	rst["message"] = s
 	return rst
 }
