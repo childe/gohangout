b0810dfc12751be0e5847053f0acb128a8508467
childe <heychilde@gmail.com>
2018-02-11 15:10:51 +0800

    set @timestamp as time.Time

--- codec/plain_decoder.go
+++ codec/plain_decoder.go
@@ -7,7 +7,7 @@ type PlainDecoder struct {
 
 func (d *PlainDecoder) Decode(s string) map[string]interface{} {
 	rst := make(map[string]interface{})
-	rst["@timestamp"] = time.Now().UTC()
+	rst["@timestamp"] = time.Now()
 	rst["message"] = s
 	return rst
 }
