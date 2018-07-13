77b0bf8b0103ada20b297a628d6f5c44311a01bd
childe <heychilde@gmail.com>
2018-07-13 14:40:59 +0800

    new file:   filter/drop_test.go

---
+++ filter/drop_test.go
@@ -0,0 +1,34 @@
+package filter
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDropFilter(t *testing.T) {
+	var (
+		config map[interface{}]interface{}
+		f      *DropFilter
+		event  map[string]interface{}
+		ok     bool
+	)
+
+	// test DropFilter without any condition
+	config = make(map[interface{}]interface{})
+	f = NewDropFilter(config)
+
+	event = make(map[string]interface{})
+	event["@timestamp"] = time.Now().Unix()
+	event["first"] = "dehua"
+	event["last"] = "liu"
+
+	event, ok = f.Process(event)
+
+	if ok == false {
+		t.Error("drop filter fail")
+	}
+
+	if event != nil {
+		t.Error("event should be nil after being dropped")
+	}
+}
