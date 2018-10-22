a01e724b07f9fd5dfac6557b9b31f24682e6c7ba
childe <heychilde@gmail.com>
2018-10-22 14:28:34 +0800

    remove hermes decoder

--- codec/decoder.go
+++ codec/decoder.go
@@ -13,18 +13,6 @@ func NewDecoder(t string) Decoder {
 	if t == "plain" {
 		return &PlainDecoder{}
 	}
-	if t == "hermes2" {
-		return &HermesDecoder2{
-			MAGIC:      []byte("hems"),
-			CRC_LENGTH: 8,
-		}
-	}
-	if t == "hermes" {
-		return &HermesDecoder{
-			MAGIC:      []byte("hems"),
-			CRC_LENGTH: 8,
-		}
-	}
 	glog.Infof("no %s decoder, use plain decoder", t)
 	return &PlainDecoder{}
 }
