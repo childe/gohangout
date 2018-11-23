0ea42d7df99bf32fec604d7a9badccc12d2dc57d
childe <heychilde@gmail.com>
2018-11-23 16:02:11 +0800

    add: dot output

---
+++ output/dot_output.go
@@ -0,0 +1,21 @@
+package output
+
+import "fmt"
+
+type DotOutput struct {
+	BaseOutput
+	config map[interface{}]interface{}
+}
+
+func NewDotOutput(config map[interface{}]interface{}) *DotOutput {
+	return &DotOutput{
+		BaseOutput: NewBaseOutput(config),
+		config:     config,
+	}
+}
+
+func (outputPlugin *DotOutput) Emit(event map[string]interface{}) {
+	fmt.Printf(".")
+}
+
+func (outputPlugin *DotOutput) Shutdown() {}
