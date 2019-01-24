7b63ce7e825e2ff16160d44b6c56643b4ce2210b
childe <heychilde@gmail.com>
2019-01-24 17:02:11 +0800

    add more test case

--- test/itest.sh
+++ test/itest.sh
@@ -71,3 +71,113 @@ then
 	em_print  'tag2 should output 3000 docs!'
     exit 255
 fi
+
+em_print 'test LinkMetrcik Filter 1: seperate'
+
+(build/gohangout-test --config test/itest-6.yml && sleep 1) | build/gohangout-test --config test/itest-6-2.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "4" ]
+then
+	em_print  'metric should output 8 docs!'
+    exit 255
+fi
+
+(build/gohangout-test --config test/itest-6.yml && sleep 2) | build/gohangout-test --config test/itest-6-2.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "4" ]
+then
+	em_print  'metric should output 3 docs!'
+    exit 255
+fi
+
+wcl=`grep -v count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl raw lines in output"
+
+if [ "$wcl" != "1000" ]
+then
+	em_print  'raw should output 1000 docs!'
+    exit 255
+fi
+
+em_print 'test LinkMetrcik Filter 2: cumulative'
+
+(build/gohangout-test --config test/itest-6.yml && sleep 1) | build/gohangout-test --config test/itest-6-3.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "4" ]
+then
+	em_print  'metric should output 3 docs!'
+    exit 255
+fi
+
+(build/gohangout-test --config test/itest-6.yml && sleep 2) | build/gohangout-test --config test/itest-6-3.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "8" ]
+then
+	em_print  'metric should output 8 docs!'
+    exit 255
+fi
+
+wcl=`grep -v count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl raw lines in output"
+
+if [ "$wcl" != "1000" ]
+then
+	em_print  'raw should output 1000 docs!'
+    exit 255
+fi
+
+em_print 'test LinkMetrcik Filter 3: seperate'
+
+(build/gohangout-test --config test/itest-6.yml && sleep 2 && build/gohangout-test --config test/itest-6.yml && sleep 2) | build/gohangout-test --config test/itest-6-2.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "8" ]
+then
+	em_print  'metric should output 8 docs!'
+    exit 255
+fi
+
+wcl=`grep -v count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl raw lines in output"
+
+if [ "$wcl" != "2000" ]
+then
+	em_print  'raw should output 2000 docs!'
+    exit 255
+fi
+
+em_print 'test LinkMetrcik Filter 4: cumulative'
+
+(build/gohangout-test --config test/itest-6.yml && sleep 1 && build/gohangout-test --config test/itest-6.yml && sleep 2) | build/gohangout-test --config test/itest-6-4.yml > test/gohangout-test.output.txt
+
+wcl=`grep count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl metric lines in output"
+
+if [ "$wcl" != "20" ]
+then
+	em_print  'metric should output 20 docs!'
+    exit 255
+fi
+
+wcl=`grep -v count test/gohangout-test.output.txt | wc -l | awk '{print $1}'`
+echo  "$wcl raw lines in output"
+
+if [ "$wcl" != "2000" ]
+then
+	em_print  'raw should output 2000 docs!'
+    exit 255
+fi
