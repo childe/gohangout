29f7b9f6672556d4966e8656072b27f04c465355
childe <heychilde@gmail.com>
2019-02-25 15:14:17 +0800

    flush after every write

--- output/tcp_output.go
+++ output/tcp_output.go
@@ -56,6 +56,7 @@ func (p *TCPOutput) Emit(event map[string]interface{}) {
 	if n != len(buf) {
 		glog.Errorf("write to %s error: %s", p.address, err)
 	}
+	p.writer.Flush()
 }
 
 func (p *TCPOutput) Shutdown() {
