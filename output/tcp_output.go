6e256476821bcceb00b63bffc48e08485f2d07f9
childe <heychilde@gmail.com>
2019-03-01 11:35:01 +0800

    each tcp conn consume its own messages from chan

--- output/tcp_output.go
+++ output/tcp_output.go
@@ -3,7 +3,6 @@ package output
 import (
 	"io"
 	"net"
-	"strings"
 	"sync"
 	"time"
 
@@ -19,8 +18,10 @@ type TCPOutput struct {
 	timeout   time.Duration
 	keepalive time.Duration
 
+	concurrent int
+	messages   chan map[string]interface{}
+	conn       []net.Conn
 	//writer *bufio.Writer
-	conn net.Conn
 
 	dialLock sync.Mutex
 }
@@ -29,6 +30,7 @@ func NewTCPOutput(config map[interface{}]interface{}) *TCPOutput {
 	p := &TCPOutput{
 		BaseOutput: NewBaseOutput(config),
 		config:     config,
+		concurrent: 1,
 	}
 
 	p.network = "tcp"
@@ -55,69 +57,78 @@ func NewTCPOutput(config map[interface{}]interface{}) *TCPOutput {
 		p.keepalive = time.Second * time.Duration(keepalive)
 	}
 
-	p.loopDial()
+	if v, ok := config["concurrent"]; ok {
+		p.concurrent = v.(int)
+	}
+	p.messages = make(chan map[string]interface{}, p.concurrent)
+	p.conn = make([]net.Conn, p.concurrent)
+
+	for i := 0; i < p.concurrent; i++ {
+		go func(i int) {
+			p.conn[i] = p.loopDial()
+			for {
+				event := <-p.messages
+				d := &simplejson.SimpleJsonDecoder{}
+				buf, err := d.Encode(event)
+				if err != nil {
+					glog.Errorf("marshal %v error:%s", event, err)
+					return
+				}
+
+				buf = append(buf, '\n')
+				if err = write(p.conn[i], buf); err != nil {
+					p.conn[i].Close()
+					p.conn[i] = p.loopDial()
+				}
+			}
+		}(i)
+	}
 
 	return p
 }
 
-func (p *TCPOutput) loopDial() {
-	glog.Infof("%p loop dial", p)
-	p.dialLock.Lock()
-	defer p.dialLock.Unlock()
+func (p *TCPOutput) loopDial() net.Conn {
 	for {
-		if err := p.dial(); err != nil {
+		if conn, err := p.dial(); err != nil {
 			glog.Errorf("dial error: %s. sleep 10s", err)
 			time.Sleep(10 * time.Second)
 		} else {
-			glog.Infof("conn built to %s", p.conn.RemoteAddr())
-			return
+			glog.Infof("conn built to %s", conn.RemoteAddr())
+			return conn
 		}
 	}
 }
 
-func (p *TCPOutput) dial() error {
+func (p *TCPOutput) dial() (net.Conn, error) {
 	var d net.Dialer
 	d.Timeout = p.timeout
 	d.KeepAlive = p.keepalive
 
 	conn, err := net.Dial(p.network, p.address)
 	if err != nil {
-		return err
+		return conn, err
 	}
-	p.conn = conn
-	go p.probe()
+	// *TcpConn is net.Conn interface, so we can pass conn instead of &conn
+	go probe(conn)
 	//p.writer = bufio.NewWriter(conn)
 
-	return nil
+	return conn, nil
 }
 
-func (p *TCPOutput) probe() {
+func probe(conn net.Conn) {
 	var b = make([]byte, 1)
 
-	p.conn.SetDeadline(time.Time{})
-	p.conn.SetReadDeadline(time.Time{})
-	_, err := p.conn.Read(b) // should block here
+	conn.SetDeadline(time.Time{})
+	conn.SetReadDeadline(time.Time{})
+	_, err := conn.Read(b) // should block here
 	if err != nil && err == io.EOF {
-		glog.Infof("conn [%s] is closed by the server, close the conn.", p.conn.RemoteAddr())
-		p.conn.Close()
+		glog.Infof("conn [%s] is closed by the server, close the conn.", conn.RemoteAddr())
+		conn.Close()
 	}
 }
 
 func (p *TCPOutput) Emit(event map[string]interface{}) {
-	d := &simplejson.SimpleJsonDecoder{}
-	buf, err := d.Encode(event)
-	if err != nil {
-		glog.Errorf("marshal %v error:%s", event, err)
-		return
-	}
-
-	buf = append(buf, '\n')
-
-	p.write(buf) // always write \n, no matter if error occures here
-
-	//buf = []byte{'\n'}
-	//p.write(buf)
-
+	p.messages <- event
 	//buf = append(buf, '\n')
 	//n, err := p.writer.Write(buf)
 	//if n != len(buf) {
@@ -126,23 +137,21 @@ func (p *TCPOutput) Emit(event map[string]interface{}) {
 	//p.writer.Flush()
 }
 
-func (p *TCPOutput) write(buf []byte) error {
+func write(conn net.Conn, buf []byte) error {
 	for len(buf) > 0 {
-		n, err := p.conn.Write(buf)
+		n, err := conn.Write(buf)
 		if err != nil {
-			glog.Errorf("write to %s[%s] error: %s", p.address, p.conn.RemoteAddr(), err)
-			str := err.Error()
-			glog.Info(str)
-			glog.Infof("%d %s", len(buf), string(buf))
-			switch {
-			case strings.Contains(str, "use of closed network connection"):
-				p.loopDial()
-				return err
-			case strings.Contains(str, "write: broken pipe"):
-				p.conn.Close()
-				p.loopDial()
-				return err
-			}
+			return err
+			//glog.Errorf("write to %s[%s] error: %s", p.address, conn.RemoteAddr(), err)
+			//switch {
+			//case strings.Contains(str, "use of closed network connection"):
+			//conn = loopDial()
+			//return err
+			//case strings.Contains(str, "write: broken pipe"):
+			//conn.Close()
+			//conn = loopDial()
+			//return err
+			//}
 		}
 		buf = buf[n:]
 	}
@@ -151,5 +160,5 @@ func (p *TCPOutput) write(buf []byte) error {
 
 func (p *TCPOutput) Shutdown() {
 	//p.writer.Flush()
-	p.conn.Close()
+	//p.conn.Close()
 }
