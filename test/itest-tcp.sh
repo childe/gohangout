720de8bf96a17ab59e9c7bfc81eb0cc959a3d04a
childe <heychilde@gmail.com>
2019-03-01 11:42:19 +0800

    200000 docs

--- test/itest-tcp.sh
+++ test/itest-tcp.sh
@@ -14,16 +14,19 @@ nohup build/gohangout-test --config test/itest-tcpinput.yml > $tmpfile &
 sleep 1
 
 build/gohangout-test --config test/itest-tcpoutput.yml
+sleep 2
 
 ps -ef | grep 'build/gohangout-test --config test/itest-tcpinput.yml' | grep -v grep | awk '{print $2}' | xargs kill
 
 wcl=$(wc -l $tmpfile | awk '{print $1}')
 
 
-if [ "$wcl" != "2000" ]
+if [ "$wcl" != "200000" ]
 then
-	em_print  'tcp input/output should create 2000 docs!'
+	em_print  'tcp input/output should create 200000 docs!'
     exit 255
+else
+    em_print 'tcp plugin passes'
 fi
 
 rm $tmpfile
