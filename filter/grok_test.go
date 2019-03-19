3321e028029dda4492165616a9190b0131221f08
childe <heychilde@gmail.com>
2019-03-19 10:26:25 +0800

    增加了对grok pattern的翻译测试用例
    modified:   filter/grok_test.go

--- filter/grok_test.go
+++ filter/grok_test.go
@@ -30,3 +30,69 @@ func TestGrokFilter(t *testing.T) {
 		}
 	}
 }
+
+func TestTranslate(t *testing.T) {
+	grok := &Grok{}
+	grok.patterns = map[string]string{
+		"USERNAME": "[a-zA-Z0-9._-]+",
+		"USER":     "%{USERNAME}",
+		"IPV6":     `((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?`,
+		"IPV4":     `(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`,
+		"IP":       "(?:%{IPV6}|%{IPV4})",
+	}
+
+	p := grok.translateMatchPattern(`%{USER:user}`)
+	if p != `(?P<user>([a-zA-Z0-9._-]+))` {
+		t.Error(p)
+	}
+
+	config := make(map[interface{}]interface{})
+	match := make([]interface{}, 1)
+	match[0] = grok.translateMatchPattern(`^%{IP:ip} %{USER:user} \[(?P<loglevel>\w+)\] (?P<msg>.*)`)
+	config["match"] = match
+	config["src"] = "message"
+
+	f := NewGrokFilter(config)
+
+	event := make(map[string]interface{})
+	event["message"] = "10.10.10.255 childe [info] message"
+
+	event, ok := f.Filter(event)
+	t.Log(event)
+
+	if ok == false {
+		t.Error("grok filter fail")
+	}
+
+	if v, ok := event["loglevel"]; !ok {
+		t.Error("loglevel field should exist")
+	} else {
+		if v != "info" {
+			t.Error("loglevel field do not match")
+		}
+	}
+
+	if v, ok := event["user"]; !ok {
+		t.Error("user field should exist")
+	} else {
+		if v != "childe" {
+			t.Error("user field do not match")
+		}
+	}
+
+	if v, ok := event["ip"]; !ok {
+		t.Error("ip field should exist")
+	} else {
+		if v != "10.10.10.255" {
+			t.Error("ip field do not match")
+		}
+	}
+
+	if v, ok := event["msg"]; !ok {
+		t.Error("msg field should exist")
+	} else {
+		if v != "message" {
+			t.Error("msg field do not match")
+		}
+	}
+}
