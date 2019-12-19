697ee97cbead8f6dda4abc982dffa4d831fad6f6
childe <rmself@qq.com>
2019-12-19 16:39:09 +0800

    add .so to plugin path if not endswith it

--- output/output.go
+++ output/output.go
@@ -1,8 +1,10 @@
 package output
 
 import (
+	"fmt"
 	"plugin"
 	"reflect"
+	"strings"
 
 	"github.com/childe/gohangout/condition_filter"
 	"github.com/childe/gohangout/topology"
@@ -15,6 +17,7 @@ var methodLibrary *MethodLibrary = &MethodLibrary{}
 
 func BuildOutput(outputType string, config map[interface{}]interface{}) *topology.OutputBox {
 	var output topology.Output
+	var err error
 
 	method := reflect.ValueOf(methodLibrary).MethodByName("New" + outputType + "Output")
 	if method.IsValid() {
@@ -22,19 +25,40 @@ func BuildOutput(outputType string, config map[interface{}]interface{}) *topolog
 	} else {
 		glog.Info("use third party plugin")
 
-		p, err := plugin.Open(outputType)
-		if err != nil {
-			glog.Fatalf("could not open %s: %s", outputType, err)
+		if !strings.HasSuffix(outputType, ".so") {
+			outputType = outputType + ".so"
 		}
-		newFunc, err := p.Lookup("New")
+		output, err = getOutputFromPlugin(outputType, config)
 		if err != nil {
-			glog.Fatalf("could not find New function in %s: %s", outputType, err)
+			glog.Fatal("could not load plugin from %s. try %s.so", outputType, outputType)
 		}
-		output = newFunc.(func(map[interface{}]interface{}) interface{})(config).(topology.Output)
 	}
 
 	return &topology.OutputBox{
 		output,
 		condition_filter.NewConditionFilter(config),
+	}
+}
+
+func getOutputFromPlugin(pluginPath string, config map[interface{}]interface{}) (topology.Output, error) {
+	p, err := plugin.Open(pluginPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %s", pluginPath, err)
+	}
+	newFunc, err := p.Lookup("New")
+	if err != nil {
+		return nil, fmt.Errorf("could not find `New` function in %s: %s", pluginPath, err)
+	}
+
+	f, ok := newFunc.(func(map[interface{}]interface{}) interface{})
+	if !ok {
+		return nil, fmt.Errorf("`New` func in %s format error", pluginPath)
+	}
+
+	rst := f(config)
+	if filter, ok := rst.(topology.Output); !ok {
+		return nil, fmt.Errorf("`New` func in %s dose not return Output Interface", pluginPath)
+	} else {
+		return filter, nil
 	}
 }
