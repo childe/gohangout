ead441cf79cf5b4092f50345e298dc519f1134f8
childe <rmself@qq.com>
2020-01-14 14:45:43 +0800

    add: json_path value render

--- value_render/value_render.go
+++ value_render/value_render.go
@@ -1,19 +1,28 @@
 package value_render
 
 import (
+	"fmt"
 	"regexp"
+
+	"github.com/oliveagle/jsonpath"
 )
+
+var matchp, matchGoTemp, matchESIndex, jsonPath *regexp.Regexp
+
+func init() {
+	matchp, _ = regexp.Compile(`^(\[.*?\])+$`)
+	matchGoTemp, _ = regexp.Compile(`{{.*}}`)
+	matchESIndex, _ = regexp.Compile(`%{.*?}`) //%{+YYYY.MM.dd}
+	jsonPath, _ = regexp.Compile(`^\$\.`)
+}
 
 type ValueRender interface {
 	Render(map[string]interface{}) interface{}
 }
 
-// GetValueRender("message") return NewLiteralValueRender("message")
-func GetValueRender(template string) ValueRender {
-	matchp, _ := regexp.Compile(`^(\[.*?\])+$`)
-	matchGoTemp, _ := regexp.Compile(`{{.*}}`)
-	matchESIndex, _ := regexp.Compile(`%{.*?}`) //%{+YYYY.MM.dd}
-
+// getValueRender matches all regexp pattern and return a ValueRender
+// return nil if no pattern matched
+func getValueRender(template string) ValueRender {
 	if matchp.Match([]byte(template)) {
 		findp, _ := regexp.Compile(`(\[(.*?)\])`)
 		fields := make([]string, 0)
@@ -32,34 +41,31 @@ func GetValueRender(template string) ValueRender {
 	if matchESIndex.Match([]byte(template)) {
 		return NewIndexRender(template)
 	}
+	if jsonPath.Match([]byte(template)) {
+		pat, err := jsonpath.Compile(template)
+		if err != nil {
+			panic(fmt.Sprintf("json path compile `%s` error: %s", template, err))
+		}
+		return &JsonpathRender{pat}
+	}
 
+	return nil
+}
+
+// GetValueRender return a ValueRender, and return LiteralValueRender if no pattern matched
+func GetValueRender(template string) ValueRender {
+	r := getValueRender(template)
+	if r != nil {
+		return r
+	}
 	return NewLiteralValueRender(template)
 }
 
-// GetValueRender2("message") return OneLevelValueRender("message")
+// GetValueRender2 return a ValueRender, and return OneLevelValueRender("message") if no pattern matched
 func GetValueRender2(template string) ValueRender {
-	matchp, _ := regexp.Compile(`^(\[.*?\])+$`)
-	matchGoTemp, _ := regexp.Compile(`{{.*}}`)
-	matchESIndex, _ := regexp.Compile(`%{.*?}`) //%{+YYYY.MM.dd}
-
-	if matchp.Match([]byte(template)) {
-		findp, _ := regexp.Compile(`(\[(.*?)\])`)
-		fields := make([]string, 0)
-		for _, v := range findp.FindAllStringSubmatch(template, -1) {
-			fields = append(fields, v[2])
-		}
-
-		if len(fields) == 1 {
-			return NewOneLevelValueRender(fields[0])
-		}
-		return NewMultiLevelValueRender(fields)
+	r := getValueRender(template)
+	if r != nil {
+		return r
 	}
-	if matchGoTemp.Match([]byte(template)) {
-		return NewTemplateValueRender(template)
-	}
-	if matchESIndex.Match([]byte(template)) {
-		return NewIndexRender(template)
-	}
-
 	return NewOneLevelValueRender(template)
 }
