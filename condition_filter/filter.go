63c065a8946087b4474fcb176a259308fa2ea6c1
childe <rmself@qq.com>
2020-01-14 18:03:47 +0800

    add: ContainsCondition could use jsonpath

--- condition_filter/filter.go
+++ condition_filter/filter.go
@@ -281,15 +281,43 @@ func (c *HasSuffixCondition) Pass(event map[string]interface{}) bool {
 }
 
 type ContainsCondition struct {
+	pat       *jsonpath.Compiled
 	paths     []string
 	substring string
 }
 
-func NewContainsCondition(paths []string, substring string) *ContainsCondition {
-	return &ContainsCondition{paths, substring}
+func NewContainsCondition(c string) (*ContainsCondition, error) {
+	if strings.HasPrefix(c, `Contains($.`) {
+		p := regexp.MustCompile(`^Contains\((\$\..*),"(.*)"\)$`)
+		r := p.FindStringSubmatch(c)
+		if len(r) != 3 {
+			return nil, fmt.Errorf("split jsonpath pattern/value error in `%s`", c)
+		}
+
+		value := r[2]
+		pat, err := jsonpath.Compile(r[1])
+		if err != nil {
+			return nil, err
+		}
+
+		return &ContainsCondition{pat, nil, value}, nil
+	}
+	paths := make([]string, 0)
+	c = strings.TrimSuffix(strings.TrimPrefix(c, "Contains("), ")")
+	for _, p := range strings.Split(c, ",") {
+		paths = append(paths, strings.Trim(p, " "))
+	}
+	value := paths[len(paths)-1]
+	paths = paths[:len(paths)-1]
+	return &ContainsCondition{nil, paths, value}, nil
 }
 
 func (c *ContainsCondition) Pass(event map[string]interface{}) bool {
+	if c.pat != nil {
+		v, err := c.pat.Lookup(event)
+		return err == nil && strings.Contains(v.(string), c.substring)
+	}
+
 	var (
 		o      map[string]interface{} = event
 		length int                    = len(c.paths)
@@ -489,14 +517,7 @@ func NewSingleCondition(c string) (Condition, error) {
 
 	// Contains
 	if matched, _ := regexp.MatchString(`^Contains\(.*\)$`, c); matched {
-		paths := make([]string, 0)
-		c = strings.TrimSuffix(strings.TrimPrefix(c, "Contains("), ")")
-		for _, p := range strings.Split(c, ",") {
-			paths = append(paths, strings.Trim(p, " "))
-		}
-		value := paths[len(paths)-1]
-		paths = paths[:len(paths)-1]
-		return NewContainsCondition(paths, value), nil
+		return NewContainsCondition(c)
 	}
 
 	// ContainsAny
