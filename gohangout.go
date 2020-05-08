888935d5108919f66e90dda2249793583ca6183c
Mingkuan Li <limkuan24@gmail.com>
2020-05-09 01:28:15 +0800

    增加配置文件热更新支持

--- gohangout.go
+++ gohangout.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/childe/gohangout/input"
+	"github.com/childe/gohangout/topology"
+	"github.com/golang/glog"
+	jsoniter "github.com/json-iterator/go"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -10,16 +15,12 @@ import (
 	"runtime/pprof"
 	"sync"
 	"syscall"
-
-	"github.com/childe/gohangout/input"
-	"github.com/childe/gohangout/topology"
-	"github.com/golang/glog"
-	jsoniter "github.com/json-iterator/go"
 )
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 var options = &struct {
 	config     string
+	autoReload bool // 配置文件更新自动重启
 	pprof      bool
 	pprofAddr  string
 	cpuprofile string
@@ -38,6 +39,7 @@ var (
 
 func init() {
 	flag.StringVar(&options.config, "config", options.config, "path to configuration file or directory")
+	flag.BoolVar(&options.autoReload, "reload", options.autoReload, "if auto reload while config file changed")
 
 	flag.BoolVar(&options.pprof, "pprof", false, "if pprof")
 	flag.StringVar(&options.pprofAddr, "pprof-address", "127.0.0.1:8899", "default: 127.0.0.1:8899")
@@ -51,8 +53,15 @@ func init() {
 	printVersion()
 }
 
-func buildPluginLink(config map[string]interface{}) []*input.InputBox {
-	boxes := make([]*input.InputBox, 0)
+func buildPluginLink(config map[string]interface{}) (boxes []*input.InputBox, err error) {
+	defer func() {
+		// 一些强制类型转换出错导致panic的保护
+		if e := recover(); e != nil {
+			err = fmt.Errorf("[PANIC] %v", e)
+		}
+	}()
+
+	boxes = make([]*input.InputBox, 0)
 
 	for inputIdx, inputI := range config["inputs"].([]interface{}) {
 		var inputPlugin topology.Input
@@ -66,15 +75,27 @@ func buildPluginLink(config map[string]interface{}) []*input.InputBox {
 			inputConfig := inputConfigI.(map[interface{}]interface{})
 
 			inputPlugin = input.GetInput(inputType, inputConfig)
+			if inputPlugin == nil {
+				err = fmt.Errorf("invalid input plugin")
+				return
+			}
 
 			box := input.NewInputBox(inputPlugin, inputConfig, config)
+			if box == nil {
+				err = fmt.Errorf("new input box fail")
+				return
+			}
 			boxes = append(boxes, box)
 		}
 	}
-	return boxes
+
+	return
 }
 
 func main() {
+	// flush保证停止的时候日志都写入了文件中
+	defer glog.Flush()
+
 	if options.pprof {
 		go func() {
 			http.ListenAndServe(options.pprofAddr, nil)
@@ -105,34 +126,92 @@ func main() {
 		}()
 	}
 
-	config, err := parseConfig(options.config)
-	if err != nil {
-		glog.Fatalf("could not parse config:%s", err)
-	}
-	glog.Infof("%v", config)
-
-	boxes := buildPluginLink(config)
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		for {
-			<-c
-			signal.Stop(c)
-			for _, box := range boxes {
-				box.Shutdown()
+		// 监听配置文件更新
+		for cfg := range configChannel {
+			// 停止所有的流水线
+			StopBoxesBeat()
+			// 重新根据配置文件加载流水线
+			newBoxes, err := buildPluginLink(cfg)
+			if err == nil {
+				boxes = newBoxes
+				// 如果没报错才会执行更新
+				go StartBoxesBeat()
+			} else {
+				glog.Errorf("build plugin link fail: %s", err)
 			}
 		}
 	}()
 
+	// 初始化配置文件
+	if options.autoReload {
+		if err := watchConfig(options.config, configChannel); err != nil {
+			glog.Fatalf("watch config fail: %s", err)
+		}
+	} else {
+		config, err := parseConfig(options.config)
+		if err != nil {
+			glog.Fatalf("could not parse config:%s", err)
+		}
+		glog.Infof("%v", config)
+		configChannel <- config
+	}
+
+	ListenSignal()
+}
+
+var boxes []*input.InputBox
+var configChannel = make(chan map[string]interface{})
+
+func StartBoxesBeat() {
 	var wg sync.WaitGroup
 	wg.Add(len(boxes))
-	defer wg.Wait()
 
-	for i, _ := range boxes {
+	for i := range boxes {
 		go func(i int) {
 			defer wg.Done()
 			boxes[i].Beat(*worker)
 		}(i)
+	}
+
+	wg.Wait()
+}
+
+func StopBoxesBeat() {
+	for _, box := range boxes {
+		box.Shutdown()
+	}
+
+	boxes = make([]*input.InputBox, 0)
+}
+
+func ListenSignal() {
+	c := make(chan os.Signal, 1)
+	var stop bool
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR1)
+
+	defer glog.Infof("listen signal stop, exit...")
+
+	for sig := range c {
+		glog.Infof("capture signal: %v", sig)
+		switch sig {
+		case syscall.SIGINT, syscall.SIGTERM:
+			StopBoxesBeat()
+			close(configChannel)
+			stop = true
+		case syscall.SIGUSR1:
+			// `kill -USR1 pid`也会触发重新加载
+			config, err := parseConfig(options.config)
+			if err != nil {
+				glog.Errorf("could not parse config:%s", err)
+				continue
+			}
+			glog.Infof("%v", config)
+			configChannel <- config
+		}
+
+		if stop {
+			break
+		}
 	}
 }
