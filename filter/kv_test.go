9e011f17e392bab0d413082e3be2f037d67084a3
childe <rmself@qq.com>
2020-08-31 11:54:52 +0800

    fix #99
    建议添加KV模板的include_keys方法

--- filter/kv_test.go
+++ filter/kv_test.go
@@ -2,6 +2,42 @@ package filter
 
 import "testing"
 
+func TestIncludeKeys(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	config["field_split"] = " "
+	config["value_split"] = "="
+	config["src"] = "message"
+	config["include_keys"] = []interface{}{"a", "b", "c", "xyz"}
+	config["exclude_keys"] = []interface{}{"c"}
+	f := methodLibrary.NewKVFilter(config)
+
+	event := make(map[string]interface{})
+	event["message"] = "a=aaa b=bbb c=ccc xyz=\txyzxyz\t d=ddd"
+	t.Log(event)
+
+	event, ok := f.Filter(event)
+	if !ok {
+		t.Error("kv failed")
+	}
+	t.Log(event)
+
+	if event["a"] != "aaa" {
+		t.Error("kv failed")
+	}
+	if event["b"] != "bbb" {
+		t.Error("kv failed")
+	}
+	if _, ok := event["c"]; ok {
+		t.Error("c is excluded")
+	}
+	if event["xyz"] != "\txyzxyz\t" {
+		t.Error("kv failed")
+	}
+	if _, ok := event["d"]; ok {
+		t.Error("d is excluded")
+	}
+}
+
 func TestKVFilter(t *testing.T) {
 	config := make(map[interface{}]interface{})
 	config["field_split"] = " "
