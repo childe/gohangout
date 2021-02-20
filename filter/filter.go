145ac656f5015120cb6a679ea4f530fff4fa20dd
childe <rmself@qq.com>
2021-02-20 23:08:06 +0800

    forward compatible usage of plugin

--- filter/filter.go
+++ filter/filter.go
@@ -1,8 +1,8 @@
 package filter
 
 import (
+	"fmt"
 	"plugin"
-	"strings"
 
 	"github.com/childe/gohangout/topology"
 	"github.com/golang/glog"
@@ -28,10 +28,7 @@ func BuildFilter(filterType string, config map[interface{}]interface{}) topology
 	glog.Infof("could not load %s filter plugin, try third party plugin", filterType)
 
 	pluginPath := filterType
-	if !strings.HasSuffix(pluginPath, ".so") {
-		pluginPath = filterType + ".so"
-	}
-	_, err := plugin.Open(pluginPath)
+	_, err := getFilterFromPlugin(pluginPath, config)
 	if err != nil {
 		glog.Errorf("could not open %s: %s", pluginPath, err)
 		return nil
@@ -40,6 +37,28 @@ func BuildFilter(filterType string, config map[interface{}]interface{}) topology
 	if v, ok := registeredFilter[filterType]; ok {
 		return v(config)
 	}
-	glog.Errorf("could not load %s filter plugin", filterType)
 	return nil
+}
+
+func getFilterFromPlugin(pluginPath string, config map[interface{}]interface{}) (topology.Filter, error) {
+	p, err := plugin.Open(pluginPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %s", pluginPath, err)
+	}
+	newFunc, err := p.Lookup("New")
+	if err != nil {
+		return nil, fmt.Errorf("could not find `New` function in %s: %s", pluginPath, err)
+	}
+
+	f, ok := newFunc.(func(map[interface{}]interface{}) interface{})
+	if !ok {
+		return nil, fmt.Errorf("`New` func in %s format error", pluginPath)
+	}
+
+	rst := f(config)
+	filter, ok := rst.(topology.Filter)
+	if !ok {
+		return nil, fmt.Errorf("`New` func in %s dose not return Filter Interface", pluginPath)
+	}
+	return filter, nil
 }
