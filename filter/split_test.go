b9c3bc14b4e4584fabc348c15c7a60b519a81381
childe <rmself@qq.com>
2021-04-27 19:23:19 +0800

    split 后的数组长度可以比配置的fileds数组长

--- filter/split_test.go
+++ filter/split_test.go
@@ -2,7 +2,7 @@ package filter
 
 import "testing"
 
-func TestSplitFilter(t *testing.T) {
+func TestSplitFilter1(t *testing.T) {
 	config := make(map[interface{}]interface{})
 	fields := []interface{}{"loglevel", "date", "time", "message"}
 	config["src"] = "message"
@@ -36,20 +36,22 @@ func TestSplitFilter(t *testing.T) {
 	if event["message"] != "messages ..." {
 		t.Errorf("message error: %#v", event)
 	}
+}
 
-	config = make(map[interface{}]interface{})
-	fields = []interface{}{"loglevel", "logtime", "message"}
+func TestSplitFilter2(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	fields := []interface{}{"loglevel", "logtime", "message"}
 	config["src"] = "message"
 	config["fields"] = fields
 	config["sep"] = "] "
 	config["maxSplit"] = 3
 	config["trim"] = "[]"
-	f = BuildFilter("Split", config)
+	f := BuildFilter("Split", config)
 
-	event = make(map[string]interface{})
+	event := make(map[string]interface{})
 	event["message"] = `[INFO] [2019-03-21 23:59:59,998] messages ...`
 
-	event, ok = f.Filter(event)
+	event, ok := f.Filter(event)
 	t.Log(event)
 	if !ok {
 		t.Error("SplitFilter error")
@@ -66,23 +68,25 @@ func TestSplitFilter(t *testing.T) {
 	if event["message"] != "messages ..." {
 		t.Errorf("message error: %#v", event)
 	}
+}
 
-	// dynamic sep
-	config = make(map[interface{}]interface{})
-	fields = []interface{}{"loglevel", "logtime", "message"}
+// dynamic sep
+func TestSplitFilter3(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	fields := []interface{}{"loglevel", "date", "time", "message"}
 	config["src"] = "message"
 	config["fields"] = fields
-	config["sep"] = "] "
+	config["sep"] = "[sep]"
 	config["dynamicSep"] = true
-	config["maxSplit"] = 3
+	config["maxSplit"] = 4
 	config["trim"] = "[]"
-	f = BuildFilter("Split", config)
+	f := BuildFilter("Split", config)
 
-	event = make(map[string]interface{})
+	event := make(map[string]interface{})
 	event["message"] = `[INFO] [2019-03-21 23:59:59,998] messages ...`
 	event["sep"] = " "
 
-	event, ok = f.Filter(event)
+	event, ok := f.Filter(event)
 	t.Log(event)
 	if !ok {
 		t.Error("SplitFilter error")
@@ -92,11 +96,54 @@ func TestSplitFilter(t *testing.T) {
 		t.Errorf("loglevel error: %#v", event)
 	}
 
-	if event["logtime"] != "2019-03-21 23:59:59,998" {
+	if event["date"] != "2019-03-21" {
+		t.Errorf("logtime error: %#v", event)
+	}
+
+	if event["time"] != "23:59:59,998" {
 		t.Errorf("logtime error: %#v", event)
 	}
 
 	if event["message"] != "messages ..." {
+		t.Errorf("message error: %#v", event)
+	}
+}
+
+// length of fileds do not match length of splited
+func TestSplitFilter4(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	fields := []interface{}{"loglevel", "date", "time"}
+	config["src"] = "message"
+	config["fields"] = fields
+	config["sep"] = "[sep]"
+	config["dynamicSep"] = true
+	config["maxSplit"] = 4
+	config["trim"] = "[]"
+	f := BuildFilter("Split", config)
+
+	event := make(map[string]interface{})
+	event["message"] = `[INFO] [2019-03-21 23:59:59,998] messages ...`
+	event["sep"] = " "
+
+	event, ok := f.Filter(event)
+	t.Log(event)
+	if !ok {
+		t.Error("SplitFilter error")
+	}
+
+	if event["loglevel"] != "INFO" {
+		t.Errorf("loglevel error: %#v", event)
+	}
+
+	if event["date"] != "2019-03-21" {
+		t.Errorf("logtime error: %#v", event)
+	}
+
+	if event["time"] != "23:59:59,998" {
+		t.Errorf("logtime error: %#v", event)
+	}
+
+	if event["message"] != `[INFO] [2019-03-21 23:59:59,998] messages ...` {
 		t.Errorf("message error: %#v", event)
 	}
 }
