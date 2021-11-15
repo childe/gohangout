e7d3028c87828fdc7a7081a5d9be9d869643df7b
childe <rmself@qq.com>
2021-11-15 19:01:20 +0800

    add FormatEncoder

--- codec/encoder.go
+++ codec/encoder.go
@@ -1,7 +1,11 @@
 package codec
 
 import (
+	"plugin"
+	"strings"
+
 	"github.com/childe/gohangout/simplejson"
+	"github.com/golang/glog"
 )
 
 type Encoder interface {
@@ -15,6 +19,25 @@ func NewEncoder(t string) Encoder {
 	case "simplejson":
 		return &simplejson.SimpleJsonDecoder{}
 	}
-	panic(t + " encoder not supported")
-	return nil
+
+	// FormatEncoder
+	if strings.HasPrefix(t, "format:") {
+		splited := strings.SplitN(t, ":", 2)
+		if len(splited) != 2 {
+			glog.Fatalf("format of `%s` is incorrect", t)
+		}
+		format := splited[1]
+		return NewFormatEncoder(format)
+	}
+
+	// try plugin
+	p, err := plugin.Open(t)
+	if err != nil {
+		glog.Fatalf("could not open %s: %s", t, err)
+	}
+	newFunc, err := p.Lookup("New")
+	if err != nil {
+		glog.Fatalf("could not find New function in %s: %s", t, err)
+	}
+	return newFunc.(func() interface{})().(Encoder)
 }
