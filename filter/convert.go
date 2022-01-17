18cbe3e970233652087763bc027310dc964ad301
childe <rmself@qq.com>
2022-01-17 22:24:03 +0800

    check if string in BoolConverter

--- filter/convert.go
+++ filter/convert.go
@@ -50,7 +50,15 @@ func (c *FloatConverter) convert(v interface{}) (interface{}, error) {
 type BoolConverter struct{}
 
 func (c *BoolConverter) convert(v interface{}) (interface{}, error) {
-	return strconv.ParseBool(v.(string))
+	if v, ok := v.(string); ok {
+		rst, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, err
+		} else {
+			return rst, err
+		}
+	}
+	return nil, ErrConvertUnknownFormat
 }
 
 type StringConverter struct{}
