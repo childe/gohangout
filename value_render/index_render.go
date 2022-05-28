09e615e217259cbef1343968d44de1c59a7f798b
vlj刘佳(IT) <jia.liu@ctrip.com>
2022-05-28 19:34:14 +0800

    add: %{x}{y}-%{+} vendor

--- value_render/index_render.go
+++ value_render/index_render.go
@@ -13,6 +13,97 @@ import (
 	"github.com/golang/glog"
 )
 
+type field struct {
+	literal  bool
+	date     bool
+	value    string // used in datetime %{+} and literal
+	location *time.Location
+
+	mv *MultiLevelValueRender
+}
+
+func (f *field) render(event map[string]interface{}, location *time.Location) (string, error) {
+	if f.literal {
+		return f.value, nil
+	}
+
+	if f.date {
+		if t, ok := event["@timestamp"]; ok {
+			return dateFormat(t, f.value, location)
+		} else {
+			return dateFormat(time.Now(), f.value, location)
+		}
+	}
+	v := f.mv.Render(event)
+	if v, ok := v.(string); ok {
+		return v, nil
+	}
+	return "null", nil
+}
+
+type IndexRender struct {
+	fields   []*field
+	location *time.Location
+}
+
+// getAllFields("%{@metadata}{kafka}{topic}") => ["@metadata","kafka","topic"]
+func getAllFields(s string) []string {
+	fields := make([]string, 0)
+	r, _ := regexp.Compile(`{(.*?)}`)
+	for _, v := range r.FindAll([]byte(s), -1) {
+		fields = append(fields, string(v[1:len(v)-1]))
+	}
+	return fields
+}
+
+func NewIndexRender(t string) *IndexRender {
+	r, _ := regexp.Compile(`%({.*?})+`)
+	fields := make([]*field, 0)
+	lastPos := 0
+	for _, loc := range r.FindAllStringIndex(t, -1) {
+		s, e := loc[0], loc[1]
+		fields = append(fields, &field{
+			literal: true,
+			value:   t[lastPos:s],
+		})
+
+		if t[s+2] == '+' {
+			fields = append(fields, &field{
+				literal: false,
+				date:    true,
+				value:   t[s+3 : e-1],
+			})
+		} else {
+			fields = append(fields, &field{
+				literal: false,
+				date:    false,
+				mv:      NewMultiLevelValueRender(getAllFields(t[s+1 : e])),
+			})
+		}
+
+		lastPos = e
+	}
+
+	if lastPos < len(t) {
+		fields = append(fields, &field{
+			literal: true,
+			date:    false,
+			value:   t[lastPos:],
+		})
+	}
+	return &IndexRender{fields, time.UTC}
+}
+
+// SetTimeLocation parse `location` to time.Location ans set it as its member.
+// use this location to format time string
+func (r *IndexRender) SetTimeLocation(loc string) {
+	location, err := time.LoadLocation(loc)
+	if err != nil {
+		glog.Fatalf("invalid localtion: %s", loc)
+	}
+	r.location = location
+}
+
 func dateFormat(t interface{}, format string, location *time.Location) (string, error) {
 	if t1, ok := t.(time.Time); ok {
 		return t1.In(location).Format(format), nil
@@ -42,87 +133,13 @@ func dateFormat(t interface{}, format string, location *time.Location) (string, 
 	return format, errors.New("could not tell the type timestamp field belongs to")
 }
 
-type field struct {
-	literal bool
-	date    bool
-	value   string
-}
-
-type IndexRender struct {
-	fields   []*field
-	location *time.Location
-}
-
-func NewIndexRender(t string) *IndexRender {
-	r, _ := regexp.Compile(`%{.*?}`)
-	fields := make([]*field, 0)
-	lastPos := 0
-	for _, loc := range r.FindAllStringIndex(t, -1) {
-		s, e := loc[0], loc[1]
-		fields = append(fields, &field{
-			literal: true,
-			value:   t[lastPos:s],
-		})
-
-		if t[s+2] == '+' {
-			fields = append(fields, &field{
-				literal: false,
-				date:    true,
-				value:   t[s+3 : e-1],
-			})
-		} else {
-			fields = append(fields, &field{
-				literal: false,
-				date:    false,
-				value:   t[s+2 : e-1],
-			})
-		}
-
-		lastPos = e
-	}
-
-	if lastPos < len(t) {
-		fields = append(fields, &field{
-			literal: true,
-			date:    false,
-			value:   t[lastPos:],
-		})
-	}
-	return &IndexRender{fields, time.UTC}
-}
-
-// SetTimeLocation parse `location` to time.Location ans set it as its member.
-// use this location to format time string
-func (r *IndexRender) SetTimeLocation(loc string) {
-	location, err := time.LoadLocation(loc)
-	if err != nil {
-		glog.Fatalf("invalid localtion: %s", loc)
-	}
-	r.location = location
-}
-
 func (r *IndexRender) Render(event map[string]interface{}) interface{} {
 	fields := make([]string, len(r.fields))
 	for i, f := range r.fields {
-		if f.literal {
-			fields[i] = f.value
-			continue
-		}
-
-		if f.date {
-			if t, ok := event["@timestamp"]; ok {
-				fields[i], _ = dateFormat(t, f.value, r.location)
-			} else {
-				fields[i], _ = dateFormat(time.Now(), f.value, r.location)
-			}
+		if v, err := f.render(event, r.location); err != nil {
+			return "null"
 		} else {
-			if s, ok := event[f.value]; !ok {
-				fields[i] = "null"
-			} else {
-				if fields[i], ok = s.(string); !ok {
-					fields[i] = "null"
-				}
-			}
+			fields[i] = v
 		}
 	}
 	return strings.Join(fields, "")
--- value_render/index_render_test.go
+++ value_render/index_render_test.go
@@ -6,53 +6,103 @@ import (
 )
 
 func TestIndexRender(t *testing.T) {
+	ts, _ := time.Parse("2006-01-02", "2022-03-04")
+	for _, c := range []struct {
+		event    map[string]interface{}
+		template string
+		want     string
+	}{
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "%{+2006.01.02}",
+			want:     "2022.03.04",
+		},
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "app-%{+2006.01.02}",
+			want:     "app-2022.03.04",
+		},
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "%{+2006.01.02}-log",
+			want:     "2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "app-%{+2006.01.02}-log",
+			want:     "app-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"topic":      "topic-one",
+				"@timestamp": ts,
+			},
+			template: "app-%{topic}-%{+2006.01.02}-log",
+			want:     "app-topic-one-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "app-%{topic}-%{+2006.01.02}-log",
+			want:     "app-null-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@timestamp": ts,
+			},
+			template: "app-%{@metadata}{kafka}{topic}-%{+2006.01.02}-log",
+			want:     "app-null-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@metadata":  nil,
+				"@timestamp": ts,
+			},
+			template: "app-%{@metadata}{kafka}{topic}-%{+2006.01.02}-log",
+			want:     "app-null-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@metadata": map[string]interface{}{
+					"topic": "topic-one",
+				},
+				"@timestamp": ts,
+			},
+			template: "app-%{@metadata}{kafka}{topic}-%{+2006.01.02}-log",
+			want:     "app-null-2022.03.04-log",
+		},
+		{
+			event: map[string]interface{}{
+				"@metadata": map[string]interface{}{
+					"kafka": map[string]interface{}{
+						"topic": "topic-one",
+					},
+				},
+				"@timestamp": ts,
+			},
+			template: "app-%{@metadata}{kafka}{topic}-%{+2006.01.02}-log",
+			want:     "app-topic-one-2022.03.04-log",
+		},
+	} {
+		vr := NewIndexRender(c.template)
+		got := vr.Render(c.event).(string)
+		if c.want != got {
+			t.Errorf("render %q, want %s, got %s", c.template, c.want, got)
+		}
+	}
 	var event map[string]interface{}
 	var template string
 	var vr ValueRender
 	var indexname string
-
-	// only timestamp
-	event = make(map[string]interface{})
-	event["@timestamp"], _ = time.Parse("2006-01-02T15:04:05", "2019-03-04T14:21:00")
-
-	template = "nginx-%{+2006.01.02}"
-
-	vr = NewIndexRender(template)
-	indexname = vr.Render(event).(string)
-	t.Log(indexname)
-
-	if indexname != "nginx-2019.03.04" {
-		t.Errorf("%s != nginx-2019.03.04\n", indexname)
-	}
-
-	// only timestamp, NOT endswith timestamp
-	event = make(map[string]interface{})
-	event["@timestamp"], _ = time.Parse("2006-01-02T15:04:05", "2019-03-04T14:21:00")
-
-	template = "nginx-%{+2006.01.02}-log"
-
-	vr = NewIndexRender(template)
-	indexname = vr.Render(event).(string)
-	t.Log(indexname)
-
-	if indexname != "nginx-2019.03.04-log" {
-		t.Errorf("%s != nginx-2019.03.04-log\n", indexname)
-	}
-
-	// timestamp, appid
-	event = make(map[string]interface{})
-	event["@timestamp"], _ = time.Parse("2006-01-02T15:04:05", "2019-03-04T14:21:00")
-	event["appid"] = "100234"
-
-	template = "nginx-%{appid}-%{+2006.01.02}"
-
-	vr = NewIndexRender(template)
-	indexname = vr.Render(event).(string)
-	t.Log(indexname)
-
-	if indexname != "nginx-100234-2019.03.04" {
-		t.Errorf("%s != nginx-100234-2019.03.04\n", indexname)
-	}
 
 	// timestamp exists, appid missing
 	event = make(map[string]interface{})
